614747c86790ba44623812b4acf846cfd59f5e26
Contresito <contresito@gmail.com>
2026-01-04 22:17:30 -0300

    added dynamic import of folders

--- common/default.nix
+++ common/default.nix
@@ -1,14 +1,21 @@
 let
   # List all files in the specified subdirectory
-  allFiles = builtins.readDir ./.;
+  dirEntries = builtins.readDir ./.;
 
   # Filter and import all Nix files from the subdirectory, excluding default.nix
-  imports = map (file: import (./. + "/${file}")) (
+  fileImports = map (file: import (./. + "/${file}")) (
     builtins.filter (file: builtins.match ".*\\.nix" file != null && file != "default.nix") (
-      builtins.attrNames allFiles
+      builtins.attrNames dirEntries
     )
+  );
+
+  # Import all folders that have a default.nix inside
+  folderImports = map (folder: import (./. + "/${folder}")) (
+    builtins.filter (
+      child: dirEntries.${child} == "directory" && builtins.pathExists (./. + "/${child}/default.nix")
+    ) (builtins.attrNames dirEntries)
   );
 in
 {
-  inherit imports;
+  imports = fileImports ++ folderImports;
 }
