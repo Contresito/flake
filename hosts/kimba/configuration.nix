49c5463d28ec20013174edc84aeef34d025df313
Contresito <contresito@gmail.com>
2026-08-14 13:23:32 -0300

    add sunshine

--- hosts/kimba/configuration.nix
+++ hosts/kimba/configuration.nix
@@ -14,6 +14,17 @@
     remotePlay.openFirewall = true;
   };
 
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
+    package = pkgs.sunshine.override {
+      cudaSupport = true;
+      cudaPackages = pkgs.cudaPackages;
+    };
+  };
+
   environment.systemPackages =
     with pkgs;
     let
